cc287aac3971342ef77c6920afb6f2e8bdb0b4b9
bogosla <jdestine68@gmail.com>
2022-09-14 16:05:00 -0700

    add AuthenticationActivity for signup and login using fragment

---
+++ app/src/main/java/com/group/ddjjnews/AuthenticationActivity.java
@@ -0,0 +1,14 @@
+package com.group.ddjjnews;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AuthenticationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authentication);
+    }
+}
