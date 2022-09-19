435983cabdfcd8cca1cd39dd154f209c1eed24f2
bogosla <jdestine68@gmail.com>
2022-09-18 19:55:10 -0700

    display data in detailActivity for news

--- app/src/main/java/com/group/ddjjnews/DetailActivity.java
+++ app/src/main/java/com/group/ddjjnews/DetailActivity.java
@@ -3,9 +3,12 @@ package com.group.ddjjnews;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.group.ddjjnews.databinding.ActivityDetailBinding;
 import com.group.ddjjnews.fragments.NewsDetailFragment;
@@ -17,16 +20,25 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(DetailActivity.this, R.layout.activity_detail);
+
         setSupportActionBar(binding.detailToolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         Intent intent = getIntent();
+        Toast.makeText(this, "Outside", Toast.LENGTH_SHORT).show();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
 
         if (intent != null && intent.getStringExtra("type") != null) {
             String type = intent.getStringExtra("type");
-            getSupportFragmentManager().beginTransaction().replace(R.id.detailFrame, NewsDetailFragment.newInstance()).commit();
-        }
+            Toast.makeText(this, "Inside", Toast.LENGTH_SHORT).show();
+            Fragment fr = NewsDetailFragment.newInstance(intent.getParcelableExtra("item"));
+            fragmentManager.beginTransaction().add(R.id.detailFrame, fr).show(fr).commit();
 
+            // getSupportFragmentManager().beginTransaction().replace(R.id.detailFrame, NewsDetailFragment.newInstance(intent.getParcelableExtra("item"))).commit();
+        }
     }
+
     @Nullable
     @Override
     public Intent getSupportParentActivityIntent() {
